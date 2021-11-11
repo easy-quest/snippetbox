dc61f37b24da6fd6f255baa4c3f961c4b5394738
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-11 09:44:22 +0300

    новый файл:    SQL/snippetbox.sql

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,21 +2,17 @@ package main
 
 import (
 	"fmt"
-	"html/template" // новый импорт
-	"log"           // новый импорт
+	"html/template"
 	"net/http"
 	"strconv"
 )
 
-// Обработчик главной странице.
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) // Использование помощника notFound()
 		return
 	}
 
-	// Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
-	// файл home.page.tmpl должен быть *первым* файлом в срезе.
 	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
@@ -25,34 +21,30 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // Использование помощника serverError()
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // Использование помощника serverError()
 	}
 }
 
-//	w.Write([]byte("Привет из Snippetbox"))
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Использование помощника notFound()
 		return
 	}
 
-	fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Метод не дозволен", 405)
+		app.clientError(w, http.StatusMethodNotAllowed) // Используем помощник clientError()
 		return
 	}
 
