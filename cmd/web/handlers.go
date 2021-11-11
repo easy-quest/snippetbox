be5f3bb9c49cfef96edda0a1df9e56b6309c7ffc
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-11 13:44:47 +0300

    изменено:      cmd/web/handlers.go 	изменено:      cmd/web/main.go 	изменено:      go.mod 	новый файл:    go.sum 	новый файл:    pkg/models/models.go 	новый файл:    pkg/models/mysql/snippets.go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -38,15 +38,29 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	fmt.Fprintf(w, "Отображение определенной заметки с идентификатором %d...", id)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed) // Используем помощник clientError()
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
-	w.Write([]byte("Создание новой заметки..."))
+	// Создаем несколько переменных, содержащих тестовые данные. Мы удалим их позже.
+	title := "История про улитку"
+	content := "Улитка выползла из раковины,\nвытянула рожки,\nи опять подобрала их."
+	expires := "7"
+
+	// Передаем данные в метод SnippetModel.Insert(), получая обратно
+	// ID только что созданной записи в базу данных.
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// Перенаправляем пользователя на соответствующую страницу заметки.
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
