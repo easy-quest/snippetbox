950b45d431170c82990f15d1fb58c2e12cb15383
Ivan Yastrebov <easy-quest@mail.ru>
2021-11-11 04:51:46 +0300

    mmm

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: bin/snippetbox
---
+++ cmd/web/handlers.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"html/template" // новый импорт
+	"log"           // новый импорт
+	"net/http"
+	"strconv"
+)
+
+// Обработчик главной странице.
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
+	// файл home.page.tmpl должен быть *первым* файлом в срезе.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+//	w.Write([]byte("Привет из Snippetbox"))
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод не дозволен", 405)
+		return
+	}
+
+	w.Write([]byte("Создание новой заметки..."))
+}
